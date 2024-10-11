8ac46320a91c237a03fbcccd35ff5188dafe9199
Ben Schreiber <bpschreiber2003@gmail.com>
2024-10-10 18:30:38 -0700

    Basic spanning tree

--- src/device/switch.rs
+++ src/device/switch.rs
@@ -1,181 +1,371 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use crate::{ethernet::{interface::*, *}, is_mac_multicast_or_broadcast, mac_addr};
+use crate::{
+    ethernet::{interface::*, *},
+    is_mac_multicast_or_broadcast, mac_addr,
+};
 
 use super::cable::*;
 
-
 /// Spanning Tree Protocol (STP) Port States
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum StpPortState {
-    Discarding, // No forwarded frames, no bpdus, no learning mac addresses
+    Discarding, // No forwarded frames, receives and transmits bpdus, no learning mac addresses
     Learning,   // No forwarded frames, receives and transmits BPDUs, learning mac addresses
     Forwarding, // Forwarded frames, receives and transmits BPDUs learning mac addresses
 }
-
-
-/// Spanning Tree Protocol (STP) Port Roles
-#[derive(Debug)]
-enum StpPortRole {
-    Root,           // Lowest cost to root bridge
-    Designated,     // Best path to paticular segment
-    Alternate,      // Backup path to root bridge
-    Backup,         // Backup path to the designated port
-    Disabled,       // Port is not participating in STP
-}
-
 
 /// An ethernet interface that participates in the Spanning Tree Protocol (STP).
 #[derive(Debug)]
 struct StpPort {
     interface: EthernetInterface,
     stp_state: StpPortState,
-    stp_role: StpPortRole,
+    id: u16,
 }
 
-
 /// A layer two switch; forwards Ethernet frames to the correct interface.
-/// 
+///
 /// Implements IEEE 802.1W Rapid Spanning Tree Protocol (RSTP) to prevent loops.
+///
+/// All ports are enabled by default in the forwarding state.
 pub struct Switch {
     ports: [RefCell<StpPort>; 32],     // 32 physical ports
-    table: HashMap<MacAddress, usize>,    // maps an address to the interface it's connected to.
-    
+    table: HashMap<MacAddress, usize>, // maps an address to the interface it's connected to.
+
     // Bridge Protocol Data Unit (BPDU) fields
-    // mac_address: MacAddress,
-    // bridge_priority: u16,
+    pub mac_address: MacAddress,
+    pub bridge_priority: u16,
+    pub root_bid: u64,
+    pub root_cost: u32,
+    pub root_port: Option<usize>,
+    pub responds_to_bpdu: u32, // A bitmap that indicates which ports respond to BPDUs
 }
 
 impl Switch {
-
     /// Creates a new switch with 32 interfaces, each with a unique MAC address based on the given seed.
-    /// 
-    /// All ports are in the forwarding state by default.
-    pub fn from_seed(mac_seed: u8) -> Switch {
-
+    ///
+    /// * `mac_seed` - The seed for the MAC addresses of the interfaces. Will take the range [mac_seed, mac_seed + 32].
+    /// * `bridge_priority` - The priority of the switch in the spanning tree protocol.
+    pub fn from_seed(mac_seed: u8, bridge_priority: u16) -> Switch {
         let ports: [RefCell<StpPort>; 32] = (0..32)
-            .map(|i| RefCell::new(StpPort {
-                interface: EthernetInterface::new(mac_addr!(mac_seed + i + 1)),
-                stp_state: StpPortState::Forwarding,
-                stp_role: StpPortRole::Root,
-            }))
+            .map(|i| {
+                RefCell::new(StpPort {
+                    interface: EthernetInterface::new(mac_addr!(mac_seed + i + 1)),
+                    stp_state: StpPortState::Forwarding,
+                    id: i as u16,
+                })
+            })
             .collect::<Vec<_>>()
             .try_into()
             .unwrap();
-    
+
         Switch {
             ports,
             table: HashMap::new(),
-            // root_cost: 0,
-            // bridge_priority: 0,
-            // root_mac,
+            bridge_priority,
+            mac_address: mac_addr!(mac_seed),
+            root_bid: crate::bridge_id!(mac_addr!(mac_seed), bridge_priority),
+            root_cost: 0,
+            root_port: None,
+            responds_to_bpdu: 0,
         }
     }
 
     /// Returns a list of all the EthernetPorts connected to the switch.
     pub fn ports(&self) -> Vec<Rc<RefCell<EthernetPort>>> {
-        self.ports.iter().map(|i| i.borrow().interface.port()).collect()
+        self.ports
+            .iter()
+            .map(|i| i.borrow().interface.port())
+            .collect()
     }
 
-    /// Connects two interfaces together via EthernetPorts (bi-directional).
+    /// Connects two ports together via EthernetPorts (bi-directional).
+    /// * `port` - The port on this switch to connect.
+    /// * `other_port` - The port on the other interface to connect to.
     pub fn connect(&mut self, port: usize, other_port: &mut EthernetInterface) {
-       self.ports[port].borrow_mut().interface.connect(other_port);
+        self.ports[port].borrow_mut().interface.connect(other_port);
     }
 
-    /// Forwards all incoming frames to the correct interface based on the destination MAC address.
-    /// 
-    /// If the destination MAC address is not in the table, the frame is flooded to all interfaces (aside from the one it came from).
-    pub fn forward(&mut self) {
-        for (index, interface) in self.ports.iter().enumerate() {
-            let frames = interface.borrow_mut().interface.receive();
-            if frames.is_empty() {
-                continue;
+    /// Connects two switches ports together via EthernetPorts (bi-directional).
+    /// * `port` - The port on this switch to connect.
+    /// * `other_switch` - The other switch to connect to.
+    /// * `other_port` - The port on the other switch to connect to.
+    pub fn connect_switch(&mut self, port: usize, other_switch: &mut Switch, other_port: usize) {
+        self.ports[port]
+            .borrow_mut()
+            .interface
+            .connect(&other_switch.ports[other_port].borrow_mut().interface);
+    }
+
+    /// Initializes RSTP (Rapid Spanning Tree Protocol) on the switch by setting all ports to the Discarding state,
+    /// and sending BPDU frames to all interfaces
+    pub fn init_stp(&mut self) {
+        for port in self.ports.iter() {
+            port.borrow_mut().stp_state = StpPortState::Discarding;
+        }
+
+        let mut bpdu = BpduFrame::hello(
+            self.mac_address,
+            self.root_bid,
+            self.root_cost,
+            crate::bridge_id!(self.mac_address, self.bridge_priority),
+            0,
+        );
+
+        // Flood BPDU frames to all interfaces
+        for port in self.ports.iter() {
+            bpdu.port_id = port.borrow().id;
+            port.borrow_mut()
+                .interface
+                .send802_3(crate::mac_bpdu_addr!(), bpdu.to_bytes());
+        }
+    }
+
+    /// Finishes the initialization stage of the switch by allowing traffic from end devices.
+    pub fn finish_init_stp(&mut self) {
+        for (i, port) in self.ports.iter_mut().enumerate() {
+            let is_end_device = self.responds_to_bpdu & (1 << i) == 0;
+
+            if is_end_device || port.get_mut().stp_state == StpPortState::Learning {
+                port.get_mut().stp_state = StpPortState::Forwarding;
             }
+        }
+    }
+
+    /// Forwards incoming frames to the correct interface based on the destination MAC address.
+    /// If the destination MAC address is not in the table, the frame is flooded to all interfaces.
+    pub fn forward(&mut self) {
+        for (port, interface) in self.ports.iter().enumerate() {
+            let frames = interface.borrow_mut().interface.receive();
 
             for frame in frames {
-
-                // A source address cannot be a multicast or broadcast address
+                // Invalid address; A source address cannot be a multicast or broadcast address
                 if is_mac_multicast_or_broadcast!(frame.source_address()) {
                     continue;
                 }
 
-                // TODO: Implement STP
-                let f = match frame {
-                    EthernetFrame::Ethernet2(frame) => frame,
-                    _ => continue  // Discard non-Ethernet2 frames
-                };
+                match frame {
+                    EthernetFrame::Ethernet2(f) => {
+                        if interface.borrow().stp_state == StpPortState::Discarding {
+                            continue;
+                        }
 
-                // If the sender MAC address is not in the table, add it.
-                if !self.table.contains_key(&f.source_address) {
-                    self.table.insert(f.source_address, index);
-                }
+                        // If the sender MAC address is not in the table, add it.
+                        if !self.table.contains_key(&f.source_address) {
+                            self.table.insert(f.source_address, port);
+                        }
 
-                // If the destination MAC address is in the table, forward the mapped interface
-                if let Some(destination_index) = self.table.get(&f.destination_address) {
-                    self.ports[*destination_index].borrow_mut().interface.sendv(f.source_address, f.destination_address, f.ether_type, f.data);
-                }
+                        // If the destination MAC address is in the table, forward the mapped interface
+                        if let Some(destination_index) = self.table.get(&f.destination_address) {
+                            self.ports[*destination_index].borrow_mut().interface.sendv(
+                                f.source_address,
+                                f.destination_address,
+                                f.ether_type,
+                                f.data,
+                            );
+                            return;
+                        }
 
-                // Destination isn't in table, flood to all interfaces (except the one it came from)
-                else {
-                    for (i, other_interface) in self.ports.iter().enumerate() {
-                        if i != index {
-                            other_interface.borrow_mut().interface.sendv(f.source_address, f.destination_address, f.ether_type, f.data.clone());
+                        // Destination isn't in table, flood to all interfaces (except the one it came from)
+                        for (i, other_interface) in self.ports.iter().enumerate() {
+                            if i == port {
+                                continue;
+                            }
+
+                            other_interface.borrow_mut().interface.sendv(
+                                f.source_address,
+                                f.destination_address,
+                                f.ether_type,
+                                f.data.clone(),
+                            );
                         }
                     }
-                }
+
+                    EthernetFrame::Ethernet802_3(e802_3) => {
+                        // Ethernet 802.3 is only used for BPDU in this simulation
+                        let bpdu = match BpduFrame::from_bytes(e802_3.data) {
+                            Ok(bpdu) => bpdu,
+                            Err(_) => continue,
+                        };
+
+                        if bpdu.root_id >= self.root_bid {
+                            return;
+                        }
+
+                        self.root_bid = bpdu.root_id;
+                        self.root_cost = bpdu.root_path_cost + 1;
+                        self.root_port = Some(port);
+                        self.responds_to_bpdu |= 1 << port;
+                        self.ports[port].borrow_mut().stp_state = StpPortState::Learning;
+
+                        // Flood bpdu to all interfaces
+                        for (i, other_interface) in self.ports.iter().enumerate() {
+                            if i == port {
+                                continue;
+                            }
+
+                            let port_id = other_interface.borrow().id;
+                            other_interface.borrow_mut().interface.send802_3(
+                                crate::mac_bpdu_addr!(),
+                                BpduFrame::hello(
+                                    self.mac_address,
+                                    self.root_bid,
+                                    self.root_cost,
+                                    crate::bridge_id!(self.mac_address, self.bridge_priority),
+                                    port_id,
+                                )
+                                .to_bytes(),
+                            );
+                        }
+                    }
+                };
             }
         }
     }
 }
 
+/// BPDU MAC address for Spanning Tree Protocol
+#[macro_export]
+macro_rules! mac_bpdu_addr {
+    () => {
+        [0x01, 0x80, 0xC2, 0x00, 0x00, 0x00]
+    };
+}
 
+/// Take in a u8 array as a MAC address and a u16 as a priority to create a bridge ID.
+#[macro_export]
+macro_rules! bridge_id {
+    ($mac:expr, $priority:expr) => {{
+        let mut id: u64 = 0;
+        for &byte in $mac.iter() {
+            id = (id << 8) | (byte as u64);
+        }
+        (id << 16) | ($priority as u64)
+    }};
+}
+
+#[derive(Debug, PartialEq)]
 pub struct BpduFrame {
-    pub destination_address: MacAddress,
-    pub source_address: MacAddress,
-    pub protocol_id: u16,
-    pub version: u8,
-    pub bpdu_type: u8,
-    pub flags: u8,
-    pub root_id: u64,
-    pub root_path_cost: u32,
-    pub bridge_id: u64,
-    pub port_id: u16,
-    pub message_age: u16,
-    pub max_age: u16,
-    pub hello_time: u16,
-    pub forward_delay: u16,
+    destination_address: MacAddress,
+    source_address: MacAddress,
+    protocol_id: u16, // 0x0000 for STP, 0x0000 for RSTP
+    version: u8,      // 0x00 for STP, 0x02 for RSTP. Always 0x02 in this implementation.
+    bpdu_type: u8,    // 0x00 for Configuration BPDU, 0x02 for TCN BPDU
+    flags: u8,
+    root_id: u64,        // Bridge ID = Root MAC Address + Root Priority
+    root_path_cost: u32, // The cost of the path to the root bridge
+    bridge_id: u64,      // Bridge ID = Bridge MAC Address + Bridge Priority
+    port_id: u16,        // Port ID = Port Priority + Port Number
+    message_age: u16,
+    max_age: u16,
+    hello_time: u16,
+    forward_delay: u16,
 }
 
 impl BpduFrame {
-    pub fn new(destination_address: MacAddress, source_address: MacAddress, protocol_id: u16, version: u8, bpdu_type: u8, flags: u8, root_id: u64, root_path_cost: u32, bridge_id: u64, port_id: u16, message_age: u16, max_age: u16, hello_time: u16, forward_delay: u16) -> BpduFrame {
+    /// * `tcn` - Topology Change Notification. Set to true if the BPDU is a TCN BPDU, ie a BPDU that indicates a topology change.
+    /// * `proposal` - Set to true if the BPDU is a proposal BPDU. A proposal BPDU is sent by a designated port to the root port.
+    /// * `port_role` - The role of the port sending the BPDU. 0 = Root, 1 = Designated, 2 = Alternate, 3 = Backup, 4 = Disabled
+    /// * `learning` - Set to true if the port is in the learning state. This is the state where the port is learning MAC addresses.
+    /// * `forwarding` - Set to true if the port is in the forwarding state. This is the state where the port is forwarding frames.
+    /// * `agreement` - Set to true if the port has reached agreement with the other end of the link.
+    ///
+    /// ## Returns
+    /// A u8 representing the flags field of the BPDU frame.
+    pub fn flags(
+        tcn: bool,
+        proposal: bool,
+        port_role: u8,
+        learning: bool,
+        forwarding: bool,
+        agreement: bool,
+    ) -> u8 {
+        let mut flags = 0x00;
+
+        if tcn {
+            flags |= 0x01;
+        }
+
+        if proposal {
+            flags |= 0x02;
+        }
+
+        flags |= port_role << 2;
+
+        if learning {
+            flags |= 0x10;
+        }
+
+        if forwarding {
+            flags |= 0x20;
+        }
+
+        if agreement {
+            flags |= 0x40;
+        }
+
+        flags
+    }
+
+    pub fn new(
+        destination_address: MacAddress,
+        source_address: MacAddress,
+        config_type: bool,
+        flags: u8,
+        root_bridge_id: u64,
+        root_path_cost: u32,
+        bridge_id: u64,
+        port_id: u16,
+    ) -> BpduFrame {
+        let bpdu_type = if config_type { 0x02 } else { 0x00 };
+
         BpduFrame {
             destination_address,
             source_address,
-            protocol_id,
-            version,
-            bpdu_type,
+            protocol_id: 0x0000, // RSTP/STP
+            version: 2,          // RSTP
+            bpdu_type,           // Configuration or TCN BPDU
             flags,
-            root_id,
+            root_id: root_bridge_id,
             root_path_cost,
             bridge_id,
             port_id,
-            message_age,
-            max_age,
-            hello_time,
-            forward_delay,
+
+            // TODO: Implement timers
+            message_age: 0,
+            max_age: 0,
+            hello_time: 0,
+            forward_delay: 0,
         }
+    }
+
+    pub fn hello(
+        source_address: MacAddress,
+        root_bridge_id: u64,
+        root_path_cost: u32,
+        bridge_id: u64,
+        port_id: u16,
+    ) -> BpduFrame {
+        BpduFrame::new(
+            crate::mac_bpdu_addr!(),
+            source_address,
+            false,
+            BpduFrame::flags(false, false, 1, false, true, false),
+            root_bridge_id,
+            root_path_cost,
+            bridge_id,
+            port_id,
+        )
     }
 }
 
 impl ByteSerialize for BpduFrame {
-
     fn from_bytes(bytes: Vec<u8>) -> Result<BpduFrame, std::io::Error> {
         if bytes.len() < 35 {
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Insufficient bytes for BPDU frame; Runt frame."));
-        }
-
-        if bytes.len() > 35 {
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Oversized BPDU frame; Giant frame."));
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Insufficient bytes for BPDU frame; Runt frame.",
+            ));
         }
 
         let destination_address = bytes[0..6].try_into().unwrap();
@@ -231,12 +421,4 @@ impl ByteSerialize for BpduFrame {
 
         bytes
     }
-}
-
-/// BPDU MAC address for Spanning Tree Protocol
-#[macro_export]
-macro_rules! mac_bpdu_addr {
-    () => {
-        [0x01, 0x80, 0xC2, 0x00, 0x00, 0x00]
-    };
 }
--- src/tests/switch_tests.rs
+++ src/tests/switch_tests.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
 
 use crate::device::cable::CableSimulator;
-use crate::device::switch::Switch;
-use crate::ethernet::{interface::*, EtherType};
-use crate::{mac_addr, mac_broadcast_addr, eth2_data, eth2};
+use crate::device::switch::{BpduFrame, Switch};
+use crate::ethernet::{interface::*, ByteSerialize, EtherType, EthernetFrame};
+use crate::{bridge_id, eth2, eth2_data, mac_addr, mac_bpdu_addr, mac_broadcast_addr};
 
 #[test]
 pub fn Switch_ReceiveNotInTable_FloodsFrame() {
@@ -12,17 +12,13 @@ pub fn Switch_ReceiveNotInTable_FloodsFrame() {
     let mut i1 = EthernetInterface::new(mac_addr!(1));
     let mut i2 = EthernetInterface::new(mac_addr!(2));
     let mut i3 = EthernetInterface::new(mac_addr!(3));
-    let mut switch = Switch::from_seed(4);
+    let mut switch = Switch::from_seed(4, 1);
 
     switch.connect(0, &mut i1);
     switch.connect(1, &mut i2);
     switch.connect(2, &mut i3);
 
-    sim.adds(vec![
-        i1.port(),
-        i2.port(),
-        i3.port(),
-    ]);
+    sim.adds(vec![i1.port(), i2.port(), i3.port()]);
 
     sim.adds(switch.ports());
 
@@ -40,21 +36,26 @@ pub fn Switch_ReceiveNotInTable_FloodsFrame() {
     assert!(i1_data.is_empty());
 
     assert!(i2_data.len() == 1);
-    assert_eq!(i2_data[0], eth2!(
-        i2.mac_address,
-        i1.mac_address,
-        eth2_data!(1),
-        EtherType::Debug
-    ));
+    assert_eq!(
+        i2_data[0],
+        eth2!(
+            i2.mac_address,
+            i1.mac_address,
+            eth2_data!(1),
+            EtherType::Debug
+        )
+    );
 
     assert!(i3_data.len() == 1);
-    assert_eq!(i3_data[0], eth2!(
-        i2.mac_address,
-        i1.mac_address,
-        eth2_data!(1),
-        EtherType::Debug
-    ));
-
+    assert_eq!(
+        i3_data[0],
+        eth2!(
+            i2.mac_address,
+            i1.mac_address,
+            eth2_data!(1),
+            EtherType::Debug
+        )
+    );
 }
 
 #[test]
@@ -64,26 +65,22 @@ pub fn Switch_ReceiveInTable_ForwardsFrame() {
     let mut i1 = EthernetInterface::new(mac_addr!(1));
     let mut i2 = EthernetInterface::new(mac_addr!(2));
     let mut i3 = EthernetInterface::new(mac_addr!(3));
-    let mut switch = Switch::from_seed(4);
+    let mut switch = Switch::from_seed(4, 1);
 
     switch.connect(0, &mut i1);
     switch.connect(1, &mut i2);
     switch.connect(2, &mut i3);
 
-    sim.adds(vec![
-        i1.port(),
-        i2.port(),
-        i3.port(),
-    ]);
+    sim.adds(vec![i1.port(), i2.port(), i3.port()]);
 
     sim.adds(switch.ports());
 
     i1.send(i2.mac_address, EtherType::Debug, eth2_data!(1));
     sim.tick();
-    switch.forward();       // Switch learns MAC address of i1
+    switch.forward(); // Switch learns MAC address of i1
     sim.tick();
-    i2.receive();           // dump incoming data
-    i3.receive();           // dump incoming data
+    i2.receive(); // dump incoming data
+    i3.receive(); // dump incoming data
 
     // Act
     i2.send(i1.mac_address, EtherType::Debug, eth2_data!(1));
@@ -96,15 +93,17 @@ pub fn Switch_ReceiveInTable_ForwardsFrame() {
 
     // Assert
     assert!(i1_data.len() == 1);
-    assert_eq!(i1_data[0], eth2!(
-        i1.mac_address,
-        i2.mac_address,
-        eth2_data!(1),
-        EtherType::Debug
-    ));
+    assert_eq!(
+        i1_data[0],
+        eth2!(
+            i1.mac_address,
+            i2.mac_address,
+            eth2_data!(1),
+            EtherType::Debug
+        )
+    );
 
     assert!(received_data3.is_empty());
-
 }
 
 #[test]
@@ -114,17 +113,13 @@ fn Switch_ReceiveBroadcastAddr_DoesNotUpdateTableAndFloodsFrame() {
     let mut i1 = EthernetInterface::new(mac_addr!(1));
     let mut i2 = EthernetInterface::new(mac_addr!(2));
     let mut i3 = EthernetInterface::new(mac_addr!(3));
-    let mut switch = Switch::from_seed(4);
+    let mut switch = Switch::from_seed(4, 1);
 
     switch.connect(0, &mut i1);
     switch.connect(1, &mut i2);
     switch.connect(2, &mut i3);
 
-    sim.adds(vec![
-        i1.port(),
-        i2.port(),
-        i3.port(),
-    ]);
+    sim.adds(vec![i1.port(), i2.port(), i3.port()]);
 
     sim.adds(switch.ports());
 
@@ -171,4 +166,138 @@ fn Switch_ReceiveBroadcastAddr_DoesNotUpdateTableAndFloodsFrame() {
 
     assert!(i3_data2.len() == 1);
     assert_eq!(i3_data2[0], frame2);
+}
+
+#[test]
+fn Switch_SpanningTree_SendsBpduDuringInitialization() {
+    // Arrange
+    let mut sim = CableSimulator::new();
+    let mut i1 = EthernetInterface::new(mac_addr!(1));
+    let mut switch = Switch::from_seed(2, 1);
+
+    let switch_port = 0;
+    switch.connect(switch_port, &mut i1);
+
+    sim.add(i1.port());
+    sim.adds(switch.ports());
+
+    // Act
+    switch.init_stp();
+    sim.tick();
+    switch.finish_init_stp();
+
+    let i1_data = i1.receive();
+
+    // Assert
+    assert!(i1_data.len() == 1);
+    let e802_3 = match &i1_data[0] {
+        EthernetFrame::Ethernet802_3(frame) => frame,
+        _ => panic!("Expected Ethernet802_3 frame"),
+    };
+
+    assert!(e802_3.destination_address == mac_bpdu_addr!());
+
+    let bpdu = match BpduFrame::from_bytes(e802_3.data.clone()) {
+        Ok(bpdu) => bpdu,
+        Err(_) => panic!("Expected BpduFrame"),
+    };
+
+    assert_eq!(
+        bpdu,
+        BpduFrame::hello(
+            switch.mac_address,
+            bridge_id!(switch.mac_address, switch.bridge_priority),
+            0,
+            bridge_id!(switch.mac_address, switch.bridge_priority),
+            switch_port as u16
+        )
+    )
+}
+
+#[test]
+fn Switch_SpanningTreeTwoSwitches_ElectsRootPortAndDesignatedPorts() {
+    // Arrange
+    let mut sim = CableSimulator::new();
+    let mut s1 = Switch::from_seed(1, 1);
+    let mut s2 = Switch::from_seed(35, 2);
+
+    let s1_port = 0;
+    let s2_port = 1;
+    s1.connect_switch(s1_port, &mut s2, s2_port);
+
+    sim.adds(s1.ports());
+    sim.adds(s2.ports());
+
+    // Act
+    s1.init_stp();
+    s2.init_stp();
+    sim.tick();
+    s1.forward();
+    s2.forward();
+    sim.tick();
+    s1.finish_init_stp();
+    s2.finish_init_stp();
+
+    // Assert
+    let root_bid = bridge_id!(s1.mac_address, s1.bridge_priority);
+    assert!(s1.root_bid == root_bid);
+    assert!(s2.root_bid == root_bid);
+    assert!(s1.root_port == None);
+    assert!(s2.root_port == Some(s2_port));
+}
+
+#[test]
+fn Switch_SpanningTree_DiscardsEndDevicesDuringInit() {
+    // Arrange
+    let mut sim = CableSimulator::new();
+    let mut i1 = EthernetInterface::new(mac_addr!(1));
+    let mut switch = Switch::from_seed(2, 1);
+
+    let switch_port = 0;
+    switch.connect(switch_port, &mut i1);
+
+    sim.add(i1.port());
+    sim.adds(switch.ports());
+
+    // Act
+    i1.send(i1.mac_address, EtherType::Debug, eth2_data!(1)); // attempt to send data to self
+    switch.init_stp();
+    sim.tick();
+    switch.forward();
+    switch.finish_init_stp();
+    sim.tick();
+
+    let i1_data = i1.receive_eth2();
+
+    // Assert
+    assert!(i1_data.is_empty());
+}
+
+#[test]
+fn Switch_SpanningTree_ForwardsEndDevicesAfterInit() {
+    // Arrange
+    let mut sim = CableSimulator::new();
+    let mut i1 = EthernetInterface::new(mac_addr!(1));
+    let mut switch = Switch::from_seed(2, 1);
+
+    let switch_port = 0;
+    switch.connect(switch_port, &mut i1);
+
+    sim.add(i1.port());
+    sim.adds(switch.ports());
+
+    // Act
+    switch.init_stp();
+    sim.tick();
+    switch.forward();
+    switch.finish_init_stp();
+    i1.send(i1.mac_address, EtherType::Debug, eth2_data!(1)); // attempt to send data to self
+    sim.tick();
+    switch.forward();
+    sim.tick();
+
+    let i1_data = i1.receive_eth2();
+
+    // Assert
+    assert!(i1_data.len() == 1);
 }
