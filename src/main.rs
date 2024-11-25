088982a513f89e71112ecd1e1caf373ece8d38f6
Ben Schreiber <bpschreiber2003@gmail.com>
2024-11-25 11:56:44 -0800

    initial simulation commit

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 #![allow(dead_code)]
 
-mod ethernet;
-mod ipv4;
-mod tick;
+mod network {
+    pub mod ethernet;
+    pub mod ipv4;
 
-mod device {
-    pub mod cable;
-    pub mod router;
-    pub mod switch;
+    pub mod device {
+        pub mod cable;
+        pub mod router;
+        pub mod switch;
+    }
+}
+
+mod simulation {
+    pub mod tick;
 }
 
 #[cfg(test)]
 mod tests {
-    pub mod cable_tests;
-    pub mod ethernet_tests;
-    pub mod ipv4_interface_tests;
-    pub mod router_tests;
-    pub mod switch_tests;
-    pub mod tick_tests;
+    mod network {
+        pub mod cable_tests;
+        pub mod ethernet_tests;
+        pub mod ipv4_interface_tests;
+        pub mod router_tests;
+        pub mod switch_tests;
+    }
+
+    mod simulation {
+        pub mod tick_tests;
+    }
 }
+use raylib::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
