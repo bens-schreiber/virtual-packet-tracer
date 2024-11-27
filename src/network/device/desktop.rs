eb2a862751695c53a61d263141457f9a7b59708b
Ben Schreiber <bpschreiber2003@gmail.com>
2024-11-26 16:51:03 -0800

    Desktop device

---
+++ src/network/device/desktop.rs
@@ -0,0 +1,30 @@
+use crate::{
+    mac_addr,
+    network::ipv4::{interface::Ipv4Interface, Ipv4Frame},
+    simulation::tick::Tickable,
+};
+
+pub struct Desktop {
+    pub interface: Ipv4Interface,
+    received: Vec<Ipv4Frame>,
+}
+
+impl Desktop {
+    pub fn from_seed(mac_seed: u64) -> Desktop {
+        let mac_addr = mac_addr!(mac_seed);
+        let ip_addr = [192, 168, 1, 1];
+        let subnet_mask = [255, 255, 255, 0];
+        let default_gateway = None;
+
+        Desktop {
+            interface: Ipv4Interface::new(mac_addr, ip_addr, subnet_mask, default_gateway),
+            received: vec![],
+        }
+    }
+}
+
+impl Tickable for Desktop {
+    fn tick(&mut self) {
+        self.received.extend(self.interface.receive());
+    }
+}
